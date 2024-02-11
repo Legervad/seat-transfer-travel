a30b96f1d05434a8c138963a84cd1bebd363279c
Emre <74266381+Legervad@users.noreply.github.com>
2024-02-11 16:04:49 +0300

    initialize backend

---
+++ backend/src/main/kotlin/com/nextbus/nextbusapi/NextbusApiApplication.kt
@@ -0,0 +1,11 @@
+package com.nextbus.nextbusapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NextbusApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NextbusApiApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/nextbus/nextbusapi/NextbusApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nextbus.nextbusapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NextbusApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
